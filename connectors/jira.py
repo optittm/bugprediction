343bfefe6ae8ec279ce75e0b099bca4553222b51
Yann Le Coz <ylcoz@icloud.com>
2022-12-20 17:04:15 +0100

    Filtered by labels

--- connectors/jira.py
+++ connectors/jira.py
@@ -15,14 +15,16 @@ class JiraConnector():
         self.__client = JIRA(server=base_url, basic_auth=(emailAddress, token))
         self.configuration = Configuration()
 
-    def _get_issues(self, labels=None):
+    def _get_issues(self, labels=["bug","enhancement"]):
         issues = []
 
         for issue in self.__client.search_issues(f"project={self.configuration.jira_project}"):
+            for label in labels:
+                issueFiltered = {"number":issue.key, "title":issue.fields.summary, "created_at":issue.fields.created}
+                
+                if label in issue.fields.labels and issueFiltered not in issues:
+                    project_id = issue.fields.project.name #KO
 
-            if "bug" in issue.fields.labels:
-                project_id = issue.fields.project.name #KO
-
-                issues.append({"number":issue.key, "title":issue.fields.summary, "created_at":issue.fields.created})
+                    issues.append(issueFiltered)
 
         return issues
