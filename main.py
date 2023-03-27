b9ec3d370b2809395618373644ca88f1ff40337d
frederic.dymko <frederic.dymko@sii.net>
2023-03-27 10:05:13 +0200

    Commenting out the Radon connector.

--- main.py
+++ main.py
@@ -306,21 +306,24 @@ def populate(ctx, skip_versions,
             # codemaat = codemaat_connector_provider(repo_dir, version)
             # codemaat.analyze_git_log()
 
-            # Get metrics with CK
-            ck = ck_connector_provider(directory=tmp_work_dir, version=version)
-            ck.analyze_source_code()
-
             # Get statistics with lizard
             lizard = file_analyzer_provider(directory=tmp_work_dir, version=version)
             lizard.analyze_source_code()
 
-            # Get metrics with JPeek
-            # jp = jpeek_connector_provider(directory=tmp_work_dir, version=version)
-            # jp.analyze_source_code()
+            if (configuration.language.lower() != "java"):
+                # Get metrics with CK
+                ck = ck_connector_provider(directory=tmp_work_dir, version=version)
+                ck.analyze_source_code()
 
-            # Get metrics with Radon
-            radon = radon_connector_provider(directory = tmp_work_dir, version = version)
-            radon.analyze_source_code()
+                # Get metrics with JPeek
+                # jp = jpeek_connector_provider(directory=tmp_work_dir, version=version)
+                # jp.analyze_source_code()
+            
+            elif (configuration.language.lower() != "python"):
+
+                # Get metrics with Radon
+                # radon = radon_connector_provider(directory = tmp_work_dir, version = version)
+                # radon.analyze_source_code()
 
     
 
