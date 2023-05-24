Fix: Correction in tests Fix: Now using scikit normalization

tests/test_math.py
@@ -4,6 +4,7 @@
 import unittest
 
 import numpy as np
+from sklearn import preprocessing
 from utils.math import Math
 
 
@@ -55,32 +56,32 @@ class TestDecisionMatrixBuilder(unittest.TestCase):
 
     def test_add_criteria(self):
         builder = Math.DecisionMatrixBuilder()
-        criteria = np.array([3, 4, 5])
+        criteria = np.array([[3, 4, 5]])
         label = "Criterion 1"
         builder.add_criteria(criteria, label)
         
         self.assertEqual(len(builder.criteria), 1)
         self.assertEqual(len(builder.criteria_label), 1)
-        self.assertTrue(np.allclose(builder.criteria[0], builder._normalize(criteria)))
+        self.assertTrue(np.allclose(builder.criteria[0], preprocessing.normalize(criteria)))
         self.assertEqual(builder.criteria_label[0], label)
 
     def test_add_alternative(self):
         builder = Math.DecisionMatrixBuilder()
-        alternative = np.array([2, 5, 1])
+        alternative = np.array([[2, 5, 1]])
         label = "Alternative 1"
         builder.add_alternative(alternative, label)
         
         self.assertEqual(len(builder.alternatives), 1)
         self.assertEqual(len(builder.alternatives_label), 1)
-        self.assertTrue(np.allclose(builder.alternatives[0], builder._normalize(alternative)))
+        self.assertTrue(np.allclose(builder.alternatives[0], preprocessing.normalize(alternative)))
         self.assertEqual(builder.alternatives_label[0], label)
 
     def test_build(self):
         builder = Math.DecisionMatrixBuilder()
-        criteria1 = np.array([3, 4, 5])
-        criteria2 = np.array([1, 2, 3])
-        alternative1 = np.array([2, 5, 1])
-        alternative2 = np.array([4, 2, 3])
+        criteria1 = np.array([[3, 4, 5]])
+        criteria2 = np.array([[1, 2, 3]])
+        alternative1 = np.array([[2, 5, 1]])
+        alternative2 = np.array([[4, 2, 3]])
         builder.add_criteria(criteria1, "Criterion 1")
         builder.add_criteria(criteria2, "Criterion 2")
         builder.add_alternative(alternative1, "Alternative 1")
@@ -89,13 +90,3 @@ class TestDecisionMatrixBuilder(unittest.TestCase):
         
         self.assertIsNotNone(matrix)
         self.assertEqual(matrix.shape, (2, 2))
-
-    def test_normalize(self):
-        builder = Math.DecisionMatrixBuilder()
-
-        matrix = np.array([1, 2, 3])
-        normalized_matrix = builder._normalize(matrix)
-
-        expected_normalized_matrix = np.array([0.26726124, 0.53452248, 0.80178373])
-
-        self.assertTrue(np.allclose(normalized_matrix, expected_normalized_matrix))

utils/math.py
@@ -2,6 +2,7 @@ from statistics import mean
 import logging
 
 import numpy as np
+from sklearn import preprocessing
 
 class Math():
     nb_decimal_numbers = 2
@@ -56,23 +57,6 @@ class Math():
             self.criteria_dict = None
             self.alternatives_dict = None
 
-        def _normalize(self, matrix: np.ndarray) -> np.ndarray:
-            """
-            Normalize a matrix using L2 norm.
-
-            Args:
-                matrix (np.ndarray): The matrix to normalize.
-
-            Returns:
-                np.ndarray: The normalized matrix.
-            """
-            norms = np.linalg.norm(matrix)
-            # if the vector is norm is 0 return the vector
-            if norms == 0:
-                return matrix
-            normalized_matrix = matrix / norms
-            return normalized_matrix
-
         def add_criteria(self, values: np.ndarray, label: str) -> 'DecisionMatrixBuilder':
             """
             Add a criteria array with its label to the decision matrix.
@@ -84,7 +68,7 @@ class Math():
             Returns:
                 DecisionMatrixBuilder: The updated instance of the DecisionMatrixBuilder.
             """
-            self.criteria.append(self._normalize(values))
+            self.criteria.append(preprocessing.normalize(values))
             self.criteria_label.append(label)
             return self
 
@@ -99,7 +83,7 @@ class Math():
             Returns:
                 DecisionMatrixBuilder: The updated instance of the DecisionMatrixBuilder.
             """
-            self.alternatives.append(self._normalize(values))
+            self.alternatives.append(preprocessing.normalize(values))
             self.alternatives_label.append(label)
             return self
 